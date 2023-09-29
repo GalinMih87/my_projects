b9c2a4954d16705848ae38b15f24ad249d2832c4
Galin Mihaylov <101407321+GalinMih87@users.noreply.github.com>
2023-09-29 11:24:20 +0300

    Add files via upload

---
+++ Calculator - value of money/future_value_annuities.py
@@ -0,0 +1,12 @@
+# Въвеждане на началния депозит, годишната лихва и периода
+initial_deposit = float(input("Въведете начален депозит: "))
+annual_interest = float(input("Въведете годишна лихва (%): "))
+period = int(input("Въведете периода (години): "))
+
+# Изчисление на бъдещата стойност на ануитета (депозита)
+annuities = ((1 + annual_interest / 100) ** period - 1) / (annual_interest / 100)
+future_value = initial_deposit * annuities
+
+# Извеждане на резултата
+print(f"Бъдещата стойност на вашия депозит след {period} години ще бъде: {future_value:.2f}")
+print(f"Aнюитета е равен на: {annuities:.4f}")
